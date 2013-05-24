7db0d117e7f51876b94df86c64229184297c2746
Matthew Rocklin <mrocklin@gmail.com>
2013-05-24 17:43:43 -0500

    squashme

--- functoolz/core.py
+++ functoolz/core.py
@@ -1,7 +1,7 @@
 def remove(predicate, coll):
     """ Return those items of collection for which predicate(item) is true.
 
-    >>> from itertoolz import remove
+    >>> from functoolz import remove
     >>> def even(x):
     ...     return x % 2 == 0
     >>> remove(even, [1, 2, 3, 4])
