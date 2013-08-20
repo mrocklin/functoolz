f3e9471bc4566b48ae57ad05edfa59d236106ad3
Matthew Rocklin <mrocklin@gmail.com>
2013-08-20 13:30:36 -0500

    add threading macros, ->, ->>

--- functoolz/core.py
+++ functoolz/core.py
@@ -1,6 +1,7 @@
 def identity(x):
     return x
 
+
 def remove(predicate, coll):
     """ Return those items of collection for which predicate(item) is true.
 
@@ -11,3 +12,75 @@ def remove(predicate, coll):
     [1, 3]
     """
     return filter(lambda x: not predicate(x), coll)
+
+
+def thread_first(val, *forms):
+    """ Thread value through a sequence of functions/forms
+
+    >>> def double(x): return 2*x
+    >>> def inc(x):    return x + 1
+    >>> thread_first(1, inc, double)
+    4
+
+    If the function expects more than one input you can specify those inputs
+    in a tuple.  The value is used as the first input.
+
+    >>> def add(x, y): return x + y
+    >>> def pow(x, y): return x**y
+    >>> thread_first(1, (add, 4), (pow, 2))  # pow(add(4, 1), 2)
+    25
+
+    So in general
+        thread_first(x, f, (g, y, z))
+    expands to
+        g(f(x), y, z)
+
+    See Also:
+        thread_last
+    """
+    def evalform_front(val, form):
+        if callable(form):
+            return form(val)
+        if isinstance(form, tuple):
+            fn, args = form[0], form[1:]
+            args = (val,) + args
+            return fn(*args)
+    return reduce(evalform_front, forms, val)
+
+
+def thread_last(val, *forms):
+    """ Thread value through a sequence of functions/forms
+
+    >>> def double(x): return 2*x
+    >>> def inc(x):    return x + 1
+    >>> thread_last(1, inc, double)
+    4
+
+    If the function expects more than one input you can specify those inputs
+    in a tuple.  The value is used as the last input.
+
+    >>> def add(x, y): return x + y
+    >>> def pow(x, y): return x**y
+    >>> thread_last(1, (add, 4), (pow, 2))  # pow(2, add(1, 4))
+    32
+
+    So in general
+        thread_last(x, f, (g, y, z))
+    expands to
+        g(y, z, f(x))
+
+    >>> def even(x): return x % 2 == 0
+    >>> thread_last([1, 2, 3], (map, inc), (filter, even))
+    [2, 4]
+
+    See Also:
+        thread_first
+    """
+    def evalform_back(val, form):
+        if callable(form):
+            return form(val)
+        if isinstance(form, tuple):
+            fn, args = form[0], form[1:]
+            args = args + (val,)
+            return fn(*args)
+    return reduce(evalform_back, forms, val)
--- functoolz/tests/test_core.py
+++ functoolz/tests/test_core.py
@@ -1,10 +1,20 @@
-from functoolz.core import remove
+from functoolz.core import remove, thread_first, thread_last
+from operator import add, mul
 
-def even(x):
-    return x % 2 == 0
-def odd(x):
-    return x % 2 == 1
+def even(x):           return x % 2 == 0
+def odd(x):            return x % 2 == 1
+def inc(x):            return x + 1
+def double(x):         return 2*x
 
 def test_remove():
     assert remove(even, range(5)) == filter(odd, range(5))
 
+def test_thread_first():
+    assert thread_first(2) == 2
+    assert thread_first(2, inc) == 3
+    assert thread_first(2, inc, inc) == 4
+    assert thread_first(2, double, inc) == 5
+    assert thread_first(2, (add, 5), double) == 14
+
+def test_thread_last():
+    assert thread_last([1, 2, 3], (map, inc), (filter, even)) == [2, 4]
